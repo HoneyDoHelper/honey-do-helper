f8b752d36e617ffcdc9a8f103a5eebd1d04424ff
AnthonyChhor <anthonychhor27@gmail.com>
2023-06-01 11:39:30 -0500

    initialization

--- src/main/java/com/codeup/honeydohelper/HoneyDoHelperApplication.java
+++ src/main/java/com/codeup/honeydohelper/HoneyDoHelperApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HoneyDoHelperApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HoneyDoHelperApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HoneyDoHelperApplication.class, args);
+    }
 
 }
