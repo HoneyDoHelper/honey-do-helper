15a30da9b463f4a4bcb2a64c61f1a100d4020903
AnthonyChhor <anthonychhor27@gmail.com>
2023-06-01 11:38:15 -0500

    added git ignore

---
+++ src/test/java/com/codeup/honeydohelper/HoneyDoHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.honeydohelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoneyDoHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
