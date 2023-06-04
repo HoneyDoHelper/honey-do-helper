8179e53e60f0ab8c06e5ea830915ceb3c76f5f4b
GageJackson <122929065+GageJackson@users.noreply.github.com>
2023-06-04 17:03:46 -0500

    more structural tweaking, login form is looking decent. index controller set up to handle entire site, will be a good starting point for each subsequent controller

--- src/main/java/com/codeup/honeydohelper/Controllers/IndexController.java
+++ src/main/java/com/codeup/honeydohelper/Controllers/IndexController.java
@@ -1,5 +1,8 @@
 package com.codeup.honeydohelper.Controllers;
 
+import com.codeup.honeydohelper.Models.*;
+import com.codeup.honeydohelper.Repositories.*;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -8,6 +11,53 @@ import java.util.*;
 
 @Controller
 public class IndexController {
+
+    private final CategoriesRepository categoriesDao;
+    private final ChatsRepository chatsDao;
+    private final ClientReviewsRepository clientReviewsDao;
+    private final HoneydoerImagesRepository honeydoerImagesDao;
+    private final HoneydoerReviewsRepository honeydoerReviewsDao;
+    private final HoneydoerSchedulesRepository honeydoerSchedulesDao;
+    private final HoneydoerServicesRepository honeydoerServicesDao;
+    private final HoneydoersRepository honeydoersDao;
+    private final ServicesRepository servicesDao;
+    private final TaskCostsRepository tasksCostsDao;
+    private final TasksRepository tasksDao;
+    private final TimeBlocksRepository timeBlocksDao;
+    private final UserDetailsRepository userDetailsDao;
+    private final UsersRepository usersDao;
+
+    public IndexController (
+            CategoriesRepository categoriesDao,
+            ChatsRepository chatsDao,
+            ClientReviewsRepository clientReviewsDao,
+            HoneydoerImagesRepository honeydoerImagesDao,
+            HoneydoerReviewsRepository honeydoerReviewsDao,
+            HoneydoerSchedulesRepository honeydoerSchedulesDao,
+            HoneydoerServicesRepository honeydoerServicesDao,
+            HoneydoersRepository honeydoersDao,
+            ServicesRepository servicesDao,
+            TaskCostsRepository tasksCostsDao,
+            TasksRepository tasksDao,
+            TimeBlocksRepository timeBlocksDao,
+            UserDetailsRepository userDetailsDao,
+            UsersRepository usersDao
+    ){
+        this.categoriesDao = categoriesDao;
+        this.chatsDao = chatsDao;
+        this.clientReviewsDao = clientReviewsDao;
+        this.honeydoerImagesDao = honeydoerImagesDao;
+        this.honeydoerReviewsDao = honeydoerReviewsDao;
+        this.honeydoerSchedulesDao = honeydoerSchedulesDao;
+        this.honeydoerServicesDao = honeydoerServicesDao;
+        this.honeydoersDao = honeydoersDao;
+        this.servicesDao = servicesDao;
+        this.tasksCostsDao = tasksCostsDao;
+        this.tasksDao = tasksDao;
+        this.timeBlocksDao = timeBlocksDao;
+        this.userDetailsDao = userDetailsDao;
+        this.usersDao = usersDao;
+    }
 
 
     /*/////////////////////////////////////////////////////////
