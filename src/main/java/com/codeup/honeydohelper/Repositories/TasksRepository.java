4f8e7c211d86f8a417f4ac0411036dd8897d5a13
GageJackson <122929065+GageJackson@users.noreply.github.com>
2023-06-05 16:28:46 -0500

    test

--- src/main/java/com/codeup/honeydohelper/Repositories/TasksRepository.java
+++ src/main/java/com/codeup/honeydohelper/Repositories/TasksRepository.java
@@ -3,5 +3,9 @@ package com.codeup.honeydohelper.Repositories;
 import com.codeup.honeydohelper.Models.Tasks;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TasksRepository extends JpaRepository<Tasks, Integer> {
+    List<Tasks> findAllByHoneydoerService_Id(int honeydoerServiceId);
+
 }
