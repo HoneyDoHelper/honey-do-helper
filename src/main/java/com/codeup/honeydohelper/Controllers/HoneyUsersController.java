39ac94906bd16b4aca123142e546005ac5322bf6
GageJackson <122929065+GageJackson@users.noreply.github.com>
2023-06-14 15:26:20 -0500

    more tweaks, log in might be fixed

--- src/main/java/com/codeup/honeydohelper/Controllers/HoneyUsersController.java
+++ src/main/java/com/codeup/honeydohelper/Controllers/HoneyUsersController.java
@@ -19,54 +19,33 @@ import java.util.Optional;
 public class HoneyUsersController {
 
     private final CategoriesRepository categoriesDao;
-    private final ChatsRepository chatsDao;
-    private final ClientReviewsRepository clientReviewsDao;
-    private final HoneydoerImagesRepository honeydoerImagesDao;
     private final HoneydoerReviewsRepository honeydoerReviewsDao;
-    private final HoneydoerSchedulesRepository honeydoerSchedulesDao;
     private final HoneydoerServicesRepository honeydoerServicesDao;
     private final HoneydoersRepository honeydoersDao;
     private final ServicesRepository servicesDao;
-    private final TaskCostsRepository tasksCostsDao;
     private final TasksRepository tasksDao;
-    private final TimeBlocksRepository timeBlocksDao;
     private final UserProfilesRepository userProfileDao;
     private final HoneyUsersRepository honeyUsersDao;
-    private PasswordEncoder passwordEncoder;
 
 
     public HoneyUsersController(
             CategoriesRepository categoriesDao,
-            ChatsRepository chatsDao,
-            ClientReviewsRepository clientReviewsDao,
-            HoneydoerImagesRepository honeydoerImagesDao,
             HoneydoerReviewsRepository honeydoerReviewsDao,
-            HoneydoerSchedulesRepository honeydoerSchedulesDao,
             HoneydoerServicesRepository honeydoerServicesDao,
             HoneydoersRepository honeydoersDao,
             ServicesRepository servicesDao,
-            TaskCostsRepository tasksCostsDao,
             TasksRepository tasksDao,
-            TimeBlocksRepository timeBlocksDao,
             UserProfilesRepository userProfileDao,
-            HoneyUsersRepository honeyUsersDao,
-            PasswordEncoder passwordEncoder
+            HoneyUsersRepository honeyUsersDao
     ) {
         this.categoriesDao = categoriesDao;
-        this.chatsDao = chatsDao;
-        this.clientReviewsDao = clientReviewsDao;
-        this.honeydoerImagesDao = honeydoerImagesDao;
         this.honeydoerReviewsDao = honeydoerReviewsDao;
-        this.honeydoerSchedulesDao = honeydoerSchedulesDao;
         this.honeydoerServicesDao = honeydoerServicesDao;
         this.honeydoersDao = honeydoersDao;
         this.servicesDao = servicesDao;
-        this.tasksCostsDao = tasksCostsDao;
         this.tasksDao = tasksDao;
-        this.timeBlocksDao = timeBlocksDao;
         this.userProfileDao = userProfileDao;
         this.honeyUsersDao = honeyUsersDao;
-        this.passwordEncoder = passwordEncoder;
     }
 
     @GetMapping("/dashboard")
--- src/main/java/com/codeup/honeydohelper/Repositories/ChatsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codeup.honeydohelper.Repositories;
-
-import com.codeup.honeydohelper.Models.Chats;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatsRepository extends JpaRepository<Chats, Integer> {
-}
